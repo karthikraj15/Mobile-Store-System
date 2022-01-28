53484af400964869f4776e1f5c1f9b6f8688de12
Karthik raj R <88207381+karthikraj15@users.noreply.github.com>
2022-01-28 23:22:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set('view engine', '.hbs');
 
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/mobile_management';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/mobile_shop';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
